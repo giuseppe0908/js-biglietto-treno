ca688366dbfac340bd5bc73a334af9504130a69a
giuseppe <placidagiuseppe@gmail.com>
2021-03-01 19:11:53 +0100

    bonus completato

---
+++ bonus/assets/main.js
@@ -0,0 +1,32 @@
+
+//Chiedere all'utente quanti km vuole percorrere e salvo la risposta in una variablie
+
+var km = parseFloat(prompt("Quanti km vuoi percorrere?"));
+
+
+//Chiedere all'utente quanti anni ha e mi salvo la risposta in una variablie
+
+var eta = parseInt(prompt("Quanti anni hai?"));
+
+var prezzo_biglietto = km * (0.21);
+// console.log(prezzo_biglietto);
+
+//Ora faccio la verifica e applico lo sconto in base al età
+
+if (eta < 18) {
+   var sconto = (prezzo_biglietto * 20) / 100 ;
+  console.log("questo è lo sconto applicato in base alla tua età"+ " "+ sconto +"€");
+  prezzo_biglietto-=sconto;
+  document.getElementById('prezzo_biglietto').innerHTML = prezzo_biglietto + "€";
+  console.log("Questo è il prezzo totale del biglietto applicando lo sconto"+ " " + prezzo_biglietto+"€");
+}
+else if (eta > 65) {
+  sconto = (prezzo_biglietto * 40) / 100;
+  console.log("questo è lo sconto applicato in base alla tua età"+ " "+  sconto+"€");
+  prezzo_biglietto-=sconto;
+    document.getElementById('prezzo_biglietto').innerHTML = prezzo_biglietto + "€";
+  console.log("Questo è il prezzo totale del biglietto applicando lo sconto" + " " + prezzo_biglietto+"€");
+}else{
+    document.getElementById('prezzo_biglietto').innerHTML = prezzo_biglietto + "€";
+  console.log("Questo è il prezzo totale del biglietto" + " " + prezzo_biglietto+"€");
+}
